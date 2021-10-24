70d7a4c31080264244c38de796d77ed8c574cc33
gisellevonbingen <gisellevonbingen@gmail.com>
2021-10-24 19:50:35 +0900

    Add thermal compat, add compat recipes

--- src/main/java/giselle/bosstools_addon/compat/CompatibleManager.java
+++ src/main/java/giselle/bosstools_addon/compat/CompatibleManager.java
@@ -5,20 +5,22 @@ import java.util.Collections;
 import java.util.List;
 
 import giselle.bosstools_addon.compat.mekanism.MekanismCompat;
+import giselle.bosstools_addon.compat.thermal.ThermalCompat;
 import giselle.bosstools_addon.compat.tinkers.TinkersCompat;
 
 public class CompatibleManager
 {
-
 	public static final List<CompatibleMod> MODS;
 	public static final TinkersCompat TINKERS;
 	public static final MekanismCompat MEKANISM;
+	public static final ThermalCompat THERMAL;
 
 	static
 	{
 		List<CompatibleMod> mods = new ArrayList<>();
 		mods.add(TINKERS = new TinkersCompat());
 		mods.add(MEKANISM = new MekanismCompat());
+		mods.add(THERMAL = new ThermalCompat());
 
 		MODS = Collections.unmodifiableList(mods);
 	}
@@ -29,5 +31,7 @@ public class CompatibleManager
 		{
 			mod.tryLoad();
 		}
+
 	}
+
 }
---
+++ src/main/java/giselle/bosstools_addon/compat/thermal/ThermalCompat.java
@@ -0,0 +1,30 @@
+package giselle.bosstools_addon.compat.thermal;
+
+import giselle.bosstools_addon.compat.CompatibleMod;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+public class ThermalCompat extends CompatibleMod
+{
+	public static final String MODID = "thermal_expansion";
+
+	public static ResourceLocation rl(String path)
+	{
+		return new ResourceLocation(MODID, path);
+	}
+
+	@Override
+	public String getModID()
+	{
+		return MODID;
+	}
+
+	@Override
+	protected void onLoad()
+	{
+		IEventBus fml_bus = FMLJavaModLoadingContext.get().getModEventBus();
+		AddonThermalItems.ITEMS.register(fml_bus);
+	}
+
+}
