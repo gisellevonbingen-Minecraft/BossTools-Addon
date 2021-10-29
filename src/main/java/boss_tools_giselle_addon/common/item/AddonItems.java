09b225ffc75d758e11717f0ce70d037c31558943
gisellevonbingen <gisellevonbingen@gmail.com>
2021-10-29 09:17:36 +0900

    Revert "remove unsing"
    This reverts commit 6a70427bd9370e939ccb78d17fd321dbfeb3ac10.

--- src/main/java/boss_tools_giselle_addon/common/item/AddonItems.java
+++ src/main/java/boss_tools_giselle_addon/common/item/AddonItems.java
@@ -18,6 +18,10 @@ public class AddonItems
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Item.class, BossToolsAddon.MODID);
 	public static final Map<RegistryObject<Block>, RegistryObject<BlockItem>> BLOCKS = new HashMap<>();
 
+	public static final RegistryObject<Item> STEEL_NUGGET = ITEMS.register("steel_nugget", () -> new Item(getMainItemProperties()));
+	public static final RegistryObject<Item> DESH_NUGGET = ITEMS.register("desh_nugget", () -> new Item(getMainItemProperties()));
+	public static final RegistryObject<Item> SILICON_NUGGET = ITEMS.register("silicon_nugget", () -> new Item(getMainItemProperties()));
+
 	static
 	{
 		for (RegistryObject<Block> blockRegistry : AddonBlocks.BLOCKS.getEntries())
--- src/main/java/boss_tools_giselle_addon/compat/CompatibleManager.java
+++ src/main/java/boss_tools_giselle_addon/compat/CompatibleManager.java
@@ -6,16 +6,19 @@ import java.util.List;
 
 import boss_tools_giselle_addon.compat.mekanism.MekanismCompat;
 import boss_tools_giselle_addon.compat.thermal.ThermalCompat;
+import boss_tools_giselle_addon.compat.tinkers.TinkersCompat;
 
 public class CompatibleManager
 {
 	public static final List<CompatibleMod> MODS;
+	public static final TinkersCompat TINKERS;
 	public static final MekanismCompat MEKANISM;
 	public static final ThermalCompat THERMAL;
 
 	static
 	{
 		List<CompatibleMod> mods = new ArrayList<>();
+		mods.add(TINKERS = new TinkersCompat());
 		mods.add(MEKANISM = new MekanismCompat());
 		mods.add(THERMAL = new ThermalCompat());
 
