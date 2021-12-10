f8e5ef6be22f93f94843cf5d6f9fe4664cfc5528
gisellevonbingen <gisellevonbingen@gmail.com>
2021-12-11 05:40:03 +0900

    Update JaopcaModule.java

--- src/main/java/boss_tools_giselle_addon/common/compat/jaopca/JaopcaModule.java
+++ src/main/java/boss_tools_giselle_addon/common/compat/jaopca/JaopcaModule.java
@@ -2,6 +2,7 @@ package boss_tools_giselle_addon.common.compat.jaopca;
 
 import java.util.Collections;
 import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -9,6 +10,7 @@ import com.google.common.collect.Sets;
 
 import boss_tools_giselle_addon.common.BossToolsAddon;
 import boss_tools_giselle_addon.common.item.crafting.AddonRecipes;
+import boss_tools_giselle_addon.common.item.crafting.ExtrudingRecipe;
 import boss_tools_giselle_addon.common.item.crafting.RollingRecipe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
@@ -31,15 +33,18 @@ import thelm.jaopca.api.modules.JAOPCAModule;
 @JAOPCAModule
 public class JaopcaModule implements IModule
 {
+	public static final HashSet<String> COMMON_BLACKLIST = Sets.newHashSet("netherite_scrap");
 	public static final Set<String> BUILTIN_COMPRESSEDS_MATERIALS = Collections.unmodifiableSet(Sets.newHashSet("steel", "desh", "silicon"));
 	public static final Set<String> BUILTIN_PLATES_MATERIALS = Collections.unmodifiableSet(Sets.newHashSet("iron", "desh"));
+	public static final Set<String> BUILTIN_RODS_MATERIALS = Collections.unmodifiableSet(Sets.newHashSet("iron"));
 
 	public static final String COMPRESSEDS_NAME = "compresseds";
 	public static final String COMPRESSEDS_TAG = BossToolsAddon.prl(COMPRESSEDS_NAME).toString();
 	public static final String COMPRESSEDS_FORM_NAME = BossToolsAddon.PMODID + "_" + COMPRESSEDS_NAME;
-	public static final Set<String> COMRESSING_BLACKLIST = Sets.union(BUILTIN_COMPRESSEDS_MATERIALS, Sets.newHashSet("netherite_scrap"));
+	public static final Set<String> COMRESSING_BLACKLIST = Sets.union(BUILTIN_COMPRESSEDS_MATERIALS, COMMON_BLACKLIST);
 
-	public static final Set<String> ROLLING_BLACKLIST = Sets.union(BUILTIN_PLATES_MATERIALS, Sets.newHashSet("netherite_scrap"));
+	public static final Set<String> ROLLING_BLACKLIST = Sets.union(BUILTIN_PLATES_MATERIALS, COMMON_BLACKLIST);
+	public static final Set<String> EXTRUDING_BLACKLIST = Sets.union(BUILTIN_RODS_MATERIALS, COMMON_BLACKLIST);
 
 	private final IForm compressedsForm;
 
@@ -79,9 +84,16 @@ public class JaopcaModule implements IModule
 
 		for (IMaterial material : moduleData.getMaterials())
 		{
-			if (ROLLING_BLACKLIST.contains(material.getName()) == false)
+			String name = material.getName();
+
+			if (ROLLING_BLACKLIST.contains(name) == false)
 			{
 				this.registerIngotRollingRecipe(material);
+			}
+
+			if (EXTRUDING_BLACKLIST.contains(name) == false)
+			{
+				this.registerIngotExtrudingRecipe(material);
 			}
 
 		}
@@ -127,6 +139,29 @@ public class JaopcaModule implements IModule
 			ItemStack output = miscHelper.getItemStack(platesTag, 1);
 
 			return new RollingRecipe(id, ingredient, output, 200);
+		});
+	}
+
+	public void registerIngotExtrudingRecipe(IMaterial material)
+	{
+		JAOPCAApi api = JAOPCAApi.instance();
+		IMiscHelper miscHelper = api.miscHelper();
+		Set<ResourceLocation> itemTags = api.getItemTags();
+		ResourceLocation ingotsTag = miscHelper.getTagLocation("ingots", material.getName());
+		ResourceLocation rodsTag = miscHelper.getTagLocation("rods", material.getName());
+
+		if (itemTags.contains(ingotsTag) == false || itemTags.contains(rodsTag) == false)
+		{
+			return;
+		}
+
+		ResourceLocation id = JaopcaCompat.rl(BossToolsAddon.MODID + "." + AddonRecipes.EXTRUDING.getName() + "." + material.getName());
+		api.registerRecipe(id, () ->
+		{
+			Ingredient ingredient = miscHelper.getIngredient(ingotsTag);
+			ItemStack output = miscHelper.getItemStack(rodsTag, 2);
+
+			return new ExtrudingRecipe(id, ingredient, output, 50);
 		});
 	}
 
