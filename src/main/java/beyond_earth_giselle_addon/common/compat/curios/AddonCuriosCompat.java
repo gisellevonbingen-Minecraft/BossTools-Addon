f61d831480b59343537da10ef65b556c05aa60bf
gisellevonbingen <gisellevonbingen@gmail.com>
2021-12-19 03:18:11 +0900

    add curio slot

--- src/main/java/beyond_earth_giselle_addon/common/compat/curios/AddonCuriosCompat.java
+++ src/main/java/beyond_earth_giselle_addon/common/compat/curios/AddonCuriosCompat.java
@@ -2,6 +2,11 @@ package beyond_earth_giselle_addon.common.compat.curios;
 
 import beyond_earth_giselle_addon.common.compat.CompatibleMod;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import top.theillusivec4.curios.api.SlotTypeMessage;
+import top.theillusivec4.curios.api.SlotTypePreset;
 
 public class AddonCuriosCompat extends CompatibleMod
 {
@@ -21,7 +26,12 @@ public class AddonCuriosCompat extends CompatibleMod
 	@Override
 	protected void onLoad()
 	{
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::registerSlots);
+	}
 
+	public void registerSlots(InterModEnqueueEvent event)
+	{
+		InterModComms.sendTo(MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.CURIO.getMessageBuilder().build());
 	}
 
 }
