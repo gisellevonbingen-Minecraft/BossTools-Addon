1094202fdbcbbe61292deb1844089dc64985b51d
gisellevonbingen <gisellevonbingen@gmail.com>
2021-12-19 03:11:44 +0900

    add curio slot

--- src/main/java/boss_tools_giselle_addon/common/compat/curios/CuriosCompat.java
+++ src/main/java/boss_tools_giselle_addon/common/compat/curios/CuriosCompat.java
@@ -2,6 +2,11 @@ package boss_tools_giselle_addon.common.compat.curios;
 
 import boss_tools_giselle_addon.common.compat.CompatibleMod;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import top.theillusivec4.curios.api.SlotTypeMessage;
+import top.theillusivec4.curios.api.SlotTypePreset;
 
 public class CuriosCompat extends CompatibleMod
 {
@@ -21,7 +26,12 @@ public class CuriosCompat extends CompatibleMod
 	@Override
 	protected void onLoad()
 	{
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::registerSlots);
+	}
 
+	public void registerSlots(InterModEnqueueEvent event)
+	{
+		InterModComms.sendTo(MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.CURIO.getMessageBuilder().build());
 	}
 
 }
