191ec04119d46f8f37e7d3fc93d9ae2739ce79c7
gisellevonbingen <gisellevonbingen@gmail.com>
2021-12-20 21:06:02 +0900

    fix jei texture

--- src/main/java/boss_tools_giselle_addon/common/compat/jei/RecipeCategoryFuelLoader.java
+++ src/main/java/boss_tools_giselle_addon/common/compat/jei/RecipeCategoryFuelLoader.java
@@ -28,7 +28,7 @@ import net.mrscauthd.boss_tools.gui.helper.GuiHelper;
 public class RecipeCategoryFuelLoader extends RecipeCategory<Fluid>
 {
 	public static final ResourceLocation BACKGROUND_LOCATION = BossToolsAddon.rl("textures/jei/fuel_loader.png");
-	public static final int BACKGROUND_WIDTH = 146;
+	public static final int BACKGROUND_WIDTH = 144;
 	public static final int BACKGROUND_HEIGHT = 84;
 	public static final int TANK_LEFT = 55;
 	public static final int TANK_TOP = 18;
