f6714c345a31419b0b7ff62bb17545835c8565ff
gisellevonbingen <gisellevonbingen@gmail.com>
2021-12-25 04:54:40 +0900

    add alien trading jei category

---
+++ src/main/java/beyond_earth_giselle_addon/common/compat/jer/AddonJeiPluginJer.java
@@ -0,0 +1,58 @@
+package beyond_earth_giselle_addon.common.compat.jer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
+import beyond_earth_giselle_addon.common.BeyondEarthAddon;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.mrscauthd.beyond_earth.entity.alien.AlienTrade;
+import net.mrscauthd.beyond_earth.entity.alien.AlienTrade.ItemListing;
+
+@mezz.jei.api.JeiPlugin
+public class AddonJeiPluginJer implements IModPlugin
+{
+	private static AddonJeiPluginJer instance;
+
+	public static AddonJeiPluginJer instance()
+	{
+		return instance;
+	}
+
+	public AddonJeiPluginJer()
+	{
+		instance = this;
+	}
+
+	@Override
+	public ResourceLocation getPluginUid()
+	{
+		return BeyondEarthAddon.rl("jer");
+	}
+
+	@Override
+	public void registerCategories(IRecipeCategoryRegistration registration)
+	{
+		registration.addRecipeCategories(new AlienCategory());
+	}
+
+	@Override
+	public void registerRecipes(IRecipeRegistration registration)
+	{
+		List<AlienWrapper> list = new ArrayList<>();
+
+		for (Entry<VillagerProfession, Int2ObjectMap<ItemListing[]>> entry : AlienTrade.TRADES.entrySet())
+		{
+			AlienEntry alienEntry = new AlienEntry(entry.getKey(), entry.getValue());
+			list.add(new AlienWrapper(alienEntry));
+		}
+
+		registration.addRecipes(list, AlienCategory.Uid);
+	}
+
+}
---
+++ src/main/java/beyond_earth_giselle_addon/common/compat/jer/AlienEntry.java
@@ -0,0 +1,112 @@
+package beyond_earth_giselle_addon.common.compat.jer;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import beyond_earth_giselle_addon.common.BeyondEarthAddon;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import jeresources.compatibility.CompatBase;
+import mezz.jei.api.recipe.IFocus;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.item.ItemStack;
+import net.mrscauthd.beyond_earth.ModInnet;
+import net.mrscauthd.beyond_earth.entity.alien.AlienEntity;
+import net.mrscauthd.beyond_earth.entity.alien.AlienTrade;
+
+public class AlienEntry
+{
+	protected final List<AlienTradeList> tradeList;
+	protected final VillagerProfession profession;
+
+	public AlienEntry(VillagerProfession profession, Int2ObjectMap<AlienTrade.ItemListing[]> tradesLists)
+	{
+		this.profession = profession;
+		this.tradeList = new LinkedList<>();
+		addItemListingLists(tradesLists);
+	}
+
+	public void addItemListingLists(Int2ObjectMap<AlienTrade.ItemListing[]> tradesLists)
+	{
+		for (int i = 1; i < tradesLists.size() + 1; i++)
+		{
+			AlienTrade.ItemListing[] levelList = tradesLists.get(i);
+			AlienTradeList trades = this.tradeList.size() > i ? this.tradeList.get(i) : new AlienTradeList(this);
+			trades.addItemListingList(levelList);
+			this.tradeList.add(trades);
+		}
+	}
+
+	public AlienTradeList getAlienTrade(int level)
+	{
+		if (tradeList.size() > level)
+		{
+			return tradeList.get(level);
+		}
+		else
+		{
+			return new AlienTradeList(null);
+		}
+	}
+
+	public List<ItemStack> getInputs()
+	{
+		List<ItemStack> list = new LinkedList<>();
+		for (List<AlienTradeList.Trade> trades : this.tradeList)
+		{
+			for (AlienTradeList.Trade trade : trades)
+			{
+				list.add(trade.getMinCostA());
+				if (!trade.getMinCostB().isEmpty())
+					list.add(trade.getMinCostB());
+			}
+		}
+		return list;
+	}
+
+	public List<ItemStack> getOutputs()
+	{
+		List<ItemStack> list = new LinkedList<>();
+		for (List<AlienTradeList.Trade> trades : this.tradeList)
+			list.addAll(trades.stream().map(AlienTradeList.Trade::getMinResult).collect(Collectors.toList()));
+		return list;
+	}
+
+	public int getMaxLevel()
+	{
+		return tradeList.size();
+	}
+
+	public String getName()
+	{
+		return this.profession.toString();
+	}
+
+	public String getDisplayName()
+	{
+		return "entity." + BeyondEarthAddon.PMODID + ".alien." + this.profession.toString();
+	}
+
+	public VillagerProfession getProfession()
+	{
+		return this.profession;
+	}
+
+	public List<Integer> getPossibleLevels(IFocus<ItemStack> focus)
+	{
+		List<Integer> levels = new ArrayList<>();
+		for (int i = 0; i < tradeList.size(); i++)
+			if (tradeList.get(i) != null && tradeList.get(i).getFocusedList(focus).size() > 0)
+				levels.add(i);
+		return levels;
+	}
+	
+	public AlienEntity getAlienEntity()
+	{
+		AlienEntity alienEntity = (AlienEntity) ModInnet.ALIEN.get().create(CompatBase.getLevel());
+		alienEntity.setVillagerData(alienEntity.getVillagerData().setProfession(this.getProfession()));
+		return alienEntity;
+	}
+
+}
