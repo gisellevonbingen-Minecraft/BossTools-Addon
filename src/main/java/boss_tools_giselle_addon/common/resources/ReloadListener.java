b31084b3c2aa75dc5c2dee31843924ef2a73fa5a
gisellevonbingen <gisellevonbingen@gmail.com>
2021-12-27 22:42:05 +0900

    Update ReloadListener.java

--- src/main/java/boss_tools_giselle_addon/common/resources/ReloadListener.java
+++ src/main/java/boss_tools_giselle_addon/common/resources/ReloadListener.java
@@ -1,18 +1,33 @@
 package boss_tools_giselle_addon.common.resources;
 
-import java.util.function.Predicate;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 import boss_tools_giselle_addon.common.item.crafting.IS2ISRecipeCache;
+import net.minecraft.profiler.IProfiler;
+import net.minecraft.resources.IFutureReloadListener;
 import net.minecraft.resources.IResourceManager;
-import net.minecraftforge.resource.IResourceType;
-import net.minecraftforge.resource.ISelectiveResourceReloadListener;
+import net.minecraft.util.Unit;
 
-public class ReloadListener implements ISelectiveResourceReloadListener
+public class ReloadListener implements IFutureReloadListener
 {
 	public static final ReloadListener INSTANCE = new ReloadListener();
 
 	@Override
-	public void onResourceManagerReload(IResourceManager resourceManager, Predicate<IResourceType> resourcePredicate)
+	public CompletableFuture<Void> reload(IStage stage, IResourceManager resourceManager, IProfiler p_215226_3_, IProfiler profiler, Executor p_215226_5_, Executor executor)
+	{
+		return stage.wait(Unit.INSTANCE).thenRunAsync(() ->
+		{
+			profiler.startTick();
+			profiler.push("listener");
+			this.onResourceManagerReload(resourceManager);
+			profiler.pop();
+			profiler.endTick();
+		}, executor);
+
+	}
+
+	public void onResourceManagerReload(IResourceManager resourceManager)
 	{
 		IS2ISRecipeCache.clearCaches();
 	}
