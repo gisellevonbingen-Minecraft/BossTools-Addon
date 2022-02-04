028d0aab4122a662a312a482cd28f55946e9ee24
gisellevonbingen <gisellevonbingen@gmail.com>
2022-02-04 15:30:03 +0900

    add command

--- src/main/java/boss_tools_giselle_addon/common/command/AddonCommand.java
+++ src/main/java/boss_tools_giselle_addon/common/command/AddonCommand.java
@@ -4,12 +4,18 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
+import boss_tools_giselle_addon.common.compat.AddonCompatibleManager;
+import boss_tools_giselle_addon.common.compat.mekanism.AddonMekanismCommand;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.text.StringTextComponent;
 import net.mrscauthd.boss_tools.ModInnet;
+import net.mrscauthd.boss_tools.capability.OxygenUtil;
 
 public class AddonCommand
 {
@@ -20,7 +26,10 @@ public class AddonCommand
 
 	public static LiteralArgumentBuilder<CommandSource> builder()
 	{
-		return Commands.literal("btga").then(PlanetSelection.builder());
+		return Commands.literal("btga") //
+				.then(PlanetSelection.builder()) //
+				.then(Equip.builder()) //
+		;
 	}
 
 	public static boolean isPlayerHasPermission(CommandSource cs, int permission)
@@ -31,6 +40,12 @@ public class AddonCommand
 	public static boolean isPlayerHasPermission2(CommandSource cs)
 	{
 		return isPlayerHasPermission(cs, 2);
+	}
+
+	public static int sendEquipedMessage(CommandSource source)
+	{
+		source.sendSuccess(new StringTextComponent("Equipped"), false);
+		return 0;
 	}
 
 	private static class PlanetSelection
@@ -50,6 +65,51 @@ public class AddonCommand
 			persistentData.putString("boss_tools:slot0", Items.AIR.getRegistryName().toString());
 
 			return 0;
+		}
+
+	}
+
+	private static class Equip
+	{
+		public static LiteralArgumentBuilder<CommandSource> builder()
+		{
+			LiteralArgumentBuilder<CommandSource> builder = Commands.literal("equip").requires(AddonCommand::isPlayerHasPermission2) //
+					.then(Commands.literal("spacesuit1").executes(Equip::spacesuit1)) //
+					.then(Commands.literal("spacesuit2").executes(Equip::spacesuit2)) //
+			;
+
+			if (AddonCompatibleManager.MEKANISM.isLoaded() == true)
+			{
+				builder.then(Commands.literal("mekasuit").executes(AddonMekanismCommand::mekasuit));
+			}
+
+			return builder;
+		}
+
+		public static int spacesuit1(CommandContext<CommandSource> context) throws CommandSyntaxException
+		{
+			CommandSource source = context.getSource();
+			ServerPlayerEntity player = source.getPlayerOrException();
+
+			player.setItemSlot(EquipmentSlotType.HEAD, new ItemStack(ModInnet.OXYGEN_MASK.get()));
+			player.setItemSlot(EquipmentSlotType.CHEST, OxygenUtil.makeFull(new ItemStack(ModInnet.SPACE_SUIT.get())));
+			player.setItemSlot(EquipmentSlotType.LEGS, new ItemStack(ModInnet.SPACE_PANTS.get()));
+			player.setItemSlot(EquipmentSlotType.FEET, new ItemStack(ModInnet.SPACE_BOOTS.get()));
+			
+			return sendEquipedMessage(source);
+		}
+
+		public static int spacesuit2(CommandContext<CommandSource> context) throws CommandSyntaxException
+		{
+			CommandSource source = context.getSource();
+			ServerPlayerEntity player = source.getPlayerOrException();
+
+			player.setItemSlot(EquipmentSlotType.HEAD, new ItemStack(ModInnet.NETHERITE_OXYGEN_MASK.get()));
+			player.setItemSlot(EquipmentSlotType.CHEST, OxygenUtil.makeFull(new ItemStack(ModInnet.NETHERITE_SPACE_SUIT.get())));
+			player.setItemSlot(EquipmentSlotType.LEGS, new ItemStack(ModInnet.NETHERITE_SPACE_PANTS.get()));
+			player.setItemSlot(EquipmentSlotType.FEET, new ItemStack(ModInnet.NETHERITE_SPACE_BOOTS.get()));
+			
+			return sendEquipedMessage(source);
 		}
 
 	}
---
+++ src/main/java/boss_tools_giselle_addon/common/compat/mekanism/AddonMekanismCommand.java
@@ -0,0 +1,56 @@
+package boss_tools_giselle_addon.common.compat.mekanism;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
+import boss_tools_giselle_addon.common.command.AddonCommand;
+import mekanism.api.gear.IModule;
+import mekanism.api.math.FloatingLong;
+import mekanism.api.providers.IModuleDataProvider;
+import mekanism.common.config.MekanismConfig;
+import mekanism.common.content.gear.shared.ModuleEnergyUnit;
+import mekanism.common.item.gear.ItemMekaSuitArmor;
+import mekanism.common.registries.MekanismItems;
+import mekanism.common.registries.MekanismModules;
+import mekanism.common.util.StorageUtils;
+import net.minecraft.command.CommandSource;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+
+public class AddonMekanismCommand
+{
+	public static int mekasuit(CommandContext<CommandSource> context) throws CommandSyntaxException
+	{
+		CommandSource source = context.getSource();
+		ServerPlayerEntity player = source.getPlayerOrException();
+
+		player.setItemSlot(EquipmentSlotType.HEAD, makeFull(MekanismItems.MEKASUIT_HELMET.get(), AddonMekanismModules.SPACE_BREATHING_UNIT));
+		player.setItemSlot(EquipmentSlotType.CHEST, makeFull(MekanismItems.MEKASUIT_BODYARMOR.get(), AddonMekanismModules.SPACE_FIRE_PROOF_UNIT, AddonMekanismModules.VENUS_ACID_PROOF_UNIT));
+		player.setItemSlot(EquipmentSlotType.LEGS, makeFull(MekanismItems.MEKASUIT_PANTS.get()));
+		player.setItemSlot(EquipmentSlotType.FEET, makeFull(MekanismItems.MEKASUIT_BOOTS.get(), AddonMekanismModules.GRAVITY_NORMALIZING_UNIT));
+		
+		return AddonCommand.sendEquipedMessage(source);
+	}
+
+	private static ItemStack makeFull(ItemMekaSuitArmor item, IModuleDataProvider<?>... moduleProviders)
+	{
+		ItemStack stack = new ItemStack(item);
+
+		for (IModuleDataProvider<?> moduleProvider : moduleProviders)
+		{
+			item.addModule(stack, moduleProvider.getModuleData());
+		}
+
+		IModule<ModuleEnergyUnit> energyUnit = item.getModule(stack, MekanismModules.ENERGY_UNIT);
+		FloatingLong maxEnergy = energyUnit == null ? MekanismConfig.gear.mekaSuitBaseEnergyCapacity.get() : energyUnit.getCustomInstance().getEnergyCapacity(energyUnit);
+
+		return StorageUtils.getFilledEnergyVariant(stack, maxEnergy);
+	}
+
+	private AddonMekanismCommand()
+	{
+
+	}
+
+}
