cc337db15383f793f4e77f0dd2a1fa8fa7b8a4f8
gisellevonbingen <gisellevonbingen@gmail.com>
2022-02-06 19:40:34 +0900

    rename classes, bump jaopca version

---
+++ src/main/java/beyond_earth_giselle_addon/common/compat/jaopca/JaopcaModuleCompatTinkers.java
@@ -0,0 +1,70 @@
+package beyond_earth_giselle_addon.common.compat.jaopca;
+
+import java.util.function.ToIntFunction;
+
+import beyond_earth_giselle_addon.common.BeyondEarthAddon;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.mrscauthd.beyond_earth.compat.tinkers.TinkersCompat;
+import slimeknights.tconstruct.library.recipe.FluidValues;
+import slimeknights.tconstruct.library.recipe.melting.IMeltingRecipe;
+import thelm.jaopca.api.JAOPCAApi;
+import thelm.jaopca.api.forms.IForm;
+import thelm.jaopca.api.helpers.IMiscHelper;
+import thelm.jaopca.api.materials.IMaterial;
+import thelm.jaopca.api.modules.IModule;
+import thelm.jaopca.api.modules.IModuleData;
+import thelm.jaopca.api.modules.JAOPCAModule;
+import thelm.jaopca.compat.tconstruct.TConstructHelper;
+
+@JAOPCAModule(modDependencies = TinkersCompat.MODID)
+public class JaopcaModuleCompatTinkers implements IModule
+{
+	public JaopcaModuleCompatTinkers()
+	{
+
+	}
+
+	@Override
+	public String getName()
+	{
+		return BeyondEarthAddon.MODID + "_compat_tinkers";
+	}
+
+	@Override
+	public void onCommonSetup(IModuleData moduleData, FMLCommonSetupEvent event)
+	{
+		JAOPCAApi api = JAOPCAApi.instance();
+		IForm compressedsForm = api.getForm(JaopcaModule.COMPRESSEDS_FORM_NAME);
+
+		for (IMaterial material : compressedsForm.getMaterials())
+		{
+			this.registerCompressedsMeltingRecipe(material, compressedsForm, JaopcaModule.COMPRESSEDS_NAME);
+		}
+
+	}
+
+	public void registerCompressedsMeltingRecipe(IMaterial material, IForm form, String suffix)
+	{
+		JAOPCAApi api = JAOPCAApi.instance();
+		IMiscHelper miscHelper = api.miscHelper();
+
+		String name = material.getName();
+		ResourceLocation compressedsTag = miscHelper.getTagLocation(JaopcaModule.COMPRESSEDS_TAG, name);
+		ResourceLocation moltenLocation = miscHelper.getTagLocation("molten", name, "_");
+
+		if (api.getItemTags().contains(compressedsTag) == false || api.getFluidTags().contains(moltenLocation) == false)
+		{
+			return;
+		}
+
+		int ingots = 1;
+		ResourceLocation id = AddonJaopcaCompat.rl(BeyondEarthAddon.PMODID + ".smeltery.melting." + name + "_" + suffix);
+		ToIntFunction<FluidStack> tempFunction = stack -> stack.getFluid().getAttributes().getTemperature(stack) - 300;
+		ToIntFunction<FluidStack> moltenTime = stack -> IMeltingRecipe.calcTime(tempFunction.applyAsInt(stack), ingots);
+		TConstructHelper tinkerHelper = TConstructHelper.INSTANCE;
+		tinkerHelper.registerMeltingRecipe(id, compressedsTag, moltenLocation, FluidValues.INGOT * ingots, tempFunction, moltenTime);
+	}
+
+}
