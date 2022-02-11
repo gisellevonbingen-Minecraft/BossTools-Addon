104f3427866536ed17858a99380a3dbe7151b308
gisellevonbingen <gisellevonbingen@gmail.com>
2022-02-12 02:26:13 +0900

    register recycle information

--- src/main/java/boss_tools_giselle_addon/common/compat/immersiveengineering/AddonIECompat.java
+++ src/main/java/boss_tools_giselle_addon/common/compat/immersiveengineering/AddonIECompat.java
@@ -1,6 +1,10 @@
 package boss_tools_giselle_addon.common.compat.immersiveengineering;
 
+import blusunrize.immersiveengineering.api.IEApi;
+import blusunrize.immersiveengineering.api.crafting.ArcRecyclingChecker;
+import boss_tools_giselle_addon.common.BossToolsAddon;
 import boss_tools_giselle_addon.common.compat.AddonCompatibleMod;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -25,6 +29,9 @@ public class AddonIECompat extends AddonCompatibleMod
 	{
 		IEventBus fml_bus = FMLJavaModLoadingContext.get().getModEventBus();
 		AddonIEItems.ITEMS.register(fml_bus);
+
+		IEApi.prefixToIngotMap.put("compresseds", new Integer[]{1, 1});
+		ArcRecyclingChecker.allowItemTagForRecycling(ItemTags.bind(BossToolsAddon.prl("compresseds").toString()));
 	}
 
 }
