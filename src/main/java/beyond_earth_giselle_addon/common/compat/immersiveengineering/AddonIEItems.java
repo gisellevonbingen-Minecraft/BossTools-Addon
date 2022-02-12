7abee10b8be04bc48ec45c24313430f35995d40f
gisellevonbingen <gisellevonbingen@gmail.com>
2022-02-12 14:13:09 +0900

    add metal_press items/recipes

---
+++ src/main/java/beyond_earth_giselle_addon/common/compat/immersiveengineering/AddonIEItems.java
@@ -0,0 +1,19 @@
+package beyond_earth_giselle_addon.common.compat.immersiveengineering;
+
+import beyond_earth_giselle_addon.common.BeyondEarthAddon;
+import beyond_earth_giselle_addon.common.item.AddonItems;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class AddonIEItems
+{
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Item.class, BeyondEarthAddon.MODID);
+	public static final RegistryObject<Item> MOLD_COMPRESSING = ITEMS.register("mold_compressing", () -> new Item(AddonItems.getMainItemProperties().stacksTo(1)));
+
+	private AddonIEItems()
+	{
+
+	}
+
+}
