50ec4e474ee9fed81da60f0e02a3c12816d4eada
gisellevonbingen <gisellevonbingen@gmail.com>
2022-02-13 23:21:22 +0900

    press compressing die stacks to 1

--- src/main/java/boss_tools_giselle_addon/common/compat/thermal/AddonThermalItems.java
+++ src/main/java/boss_tools_giselle_addon/common/compat/thermal/AddonThermalItems.java
@@ -9,7 +9,7 @@ import net.minecraftforge.registries.DeferredRegister;
 public class AddonThermalItems
 {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Item.class, BossToolsAddon.MODID);
-	public static final RegistryObject<Item> PRESS_COMPRESSING_DIE = ITEMS.register("press_compressing_die", () -> new Item(AddonItems.getMainItemProperties()));
+	public static final RegistryObject<Item> PRESS_COMPRESSING_DIE = ITEMS.register("press_compressing_die", () -> new Item(AddonItems.getMainItemProperties().stacksTo(1)));
 
 	private AddonThermalItems()
 	{
