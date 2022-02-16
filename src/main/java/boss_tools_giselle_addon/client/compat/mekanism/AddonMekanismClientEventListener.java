15e1a0f39d9925cccd60c5f358250030db9590f6
gisellevonbingen <gisellevonbingen@gmail.com>
2022-02-16 20:42:20 +0900

    improve mekanism module gravitational modulating tooltip

--- src/main/java/boss_tools_giselle_addon/client/compat/mekanism/AddonMekanismClientEventListener.java
+++ src/main/java/boss_tools_giselle_addon/client/compat/mekanism/AddonMekanismClientEventListener.java
@@ -1,17 +1,33 @@
 package boss_tools_giselle_addon.client.compat.mekanism;
 
+import java.util.List;
+
 import boss_tools_giselle_addon.common.BossToolsAddon;
 import boss_tools_giselle_addon.common.config.AddonConfigs;
 import mekanism.client.key.MekKeyHandler;
 import mekanism.client.key.MekanismKeyHandler;
+import mekanism.common.item.ItemModule;
 import mekanism.common.registries.MekanismItems;
 import net.minecraft.item.Item;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class AddonMekanismClientEventListener
 {
+	private static TranslationTextComponent CACHED_GRAVITATIONAL_MODULATING_TEXT = null;
+
+	public static ITextComponent getGravitationalModulatingModuleDescriptionText()
+	{
+		if (CACHED_GRAVITATIONAL_MODULATING_TEXT == null)
+		{
+			CACHED_GRAVITATIONAL_MODULATING_TEXT = new TranslationTextComponent(BossToolsAddon.tl("description", "instead_gravity_normalizing_unit"));
+		}
+
+		return CACHED_GRAVITATIONAL_MODULATING_TEXT;
+	}
+
 	@SubscribeEvent
 	public static void onItemTooltipEvent(ItemTooltipEvent e)
 	{
@@ -19,9 +35,31 @@ public class AddonMekanismClientEventListener
 
 		if (MekKeyHandler.isKeyPressed(MekanismKeyHandler.detailsKey) == false && AddonConfigs.Common.mekanism.moduleGravitationalModulating_normalizable.get() == true)
 		{
-			if (item == MekanismItems.MODULE_GRAVITATIONAL_MODULATING.get())
+			ItemModule moduleItem = MekanismItems.MODULE_GRAVITATIONAL_MODULATING.get();
+
+			if (item == moduleItem)
 			{
-				e.getToolTip().add(new TranslationTextComponent(BossToolsAddon.tl("description", "instead_gravity_normalizing_unit")));
+				List<ITextComponent> toolTip = e.getToolTip();
+				String descriptionKey = moduleItem.getModuleData().getDescriptionTranslationKey();
+
+				for (int i = 0; i < toolTip.size(); i++)
+				{
+					ITextComponent line = toolTip.get(i);
+
+					if (line instanceof TranslationTextComponent)
+					{
+						TranslationTextComponent tline = (TranslationTextComponent) line;
+
+						if (tline.getKey().equals(descriptionKey) == true)
+						{
+							toolTip.add(i + 1, getGravitationalModulatingModuleDescriptionText());
+							break;
+						}
+
+					}
+
+				}
+
 			}
 
 		}
