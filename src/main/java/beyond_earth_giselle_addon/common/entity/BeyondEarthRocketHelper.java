efe2b73b7ce5dbd0042cb2a331dcb6215befbace
gisellevonbingen <gisellevonbingen@gmail.com>
2022-02-18 09:41:44 +0900

    update beyond earth dependency

--- src/main/java/beyond_earth_giselle_addon/common/entity/BeyondEarthRocketHelper.java
+++ src/main/java/beyond_earth_giselle_addon/common/entity/BeyondEarthRocketHelper.java
@@ -1,14 +1,9 @@
 package beyond_earth_giselle_addon.common.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraftforge.registries.RegistryObject;
-import net.mrscauthd.beyond_earth.ModInit;
+import net.mrscauthd.beyond_earth.entity.IRocketEntity;
 import net.mrscauthd.beyond_earth.entity.RocketTier1Entity;
 import net.mrscauthd.beyond_earth.entity.RocketTier2Entity;
 import net.mrscauthd.beyond_earth.entity.RocketTier3Entity;
@@ -16,104 +11,22 @@ import net.mrscauthd.beyond_earth.entity.RocketTier4Entity;
 
 public class BeyondEarthRocketHelper
 {
-	private static final List<RegistryObject<? extends EntityType<?>>> types;
-
-	static
-	{
-		types = new ArrayList<>();
-		types.add(ModInit.TIER_1_ROCKET);
-		types.add(ModInit.TIER_2_ROCKET);
-		types.add(ModInit.TIER_3_ROCKET);
-		types.add(ModInit.TIER_4_ROCKET);
-	}
-
 	public static boolean isBeyondEarthRocket(Entity entity)
 	{
-		if (entity instanceof RocketTier1Entity)
-		{
-			return true;
-		}
-		else if (entity instanceof RocketTier2Entity)
-		{
-			return true;
-		}
-		else if (entity instanceof RocketTier3Entity)
-		{
-			return true;
-		}
-		else if (entity instanceof RocketTier4Entity)
-		{
-			return true;
-		}
-
-		return false;
-	}
-
-	public static EntityType<?>[] getBeyondEarthRocketTypes()
-	{
-		return types.stream().map(RegistryObject::get).toArray(EntityType<?>[]::new);
-	}
-
-	public static boolean isBeyondEarthRocket(EntityType<?> type)
-	{
-		for (EntityType<?> entityType : getBeyondEarthRocketTypes())
-		{
-			if (entityType == type)
-			{
-				return true;
-			}
-
-		}
-
-		return false;
+		return entity instanceof IRocketEntity;
 	}
 
 	public static int getBucketsAmount(Entity entity)
 	{
 		SynchedEntityData entityData = entity.getEntityData();
-
-		if (entity instanceof RocketTier1Entity)
-		{
-			return entityData.get(RocketTier1Entity.BUCKET) ? 1 : 0;
-		}
-		else if (entity instanceof RocketTier2Entity)
-		{
-			return entityData.get(RocketTier2Entity.BUCKETS);
-		}
-		else if (entity instanceof RocketTier3Entity)
-		{
-			return entityData.get(RocketTier3Entity.BUCKETS);
-		}
-		else if (entity instanceof RocketTier4Entity)
-		{
-			return entityData.get(RocketTier4Entity.BUCKETS);
-		}
-
-		return 0;
+		return entityData.get(IRocketEntity.BUCKETS);
 	}
 
 	public static void setBucketsAmount(Entity entity, int buckets)
 	{
 		buckets = Mth.clamp(buckets, 0, getBucketsCapacity(entity));
 		SynchedEntityData entityData = entity.getEntityData();
-
-		if (entity instanceof RocketTier1Entity)
-		{
-			entityData.set(RocketTier1Entity.BUCKET, buckets > 0);
-		}
-		else if (entity instanceof RocketTier2Entity)
-		{
-			entityData.set(RocketTier2Entity.BUCKETS, buckets);
-		}
-		else if (entity instanceof RocketTier3Entity)
-		{
-			entityData.set(RocketTier3Entity.BUCKETS, buckets);
-		}
-		else if (entity instanceof RocketTier4Entity)
-		{
-			entityData.set(RocketTier4Entity.BUCKETS, buckets);
-		}
-
+		entityData.set(IRocketEntity.BUCKETS, buckets);
 	}
 
 	public static int getBucketsCapacity(Entity entity)
@@ -141,49 +54,14 @@ public class BeyondEarthRocketHelper
 	public static int getFuelAmount(Entity entity)
 	{
 		SynchedEntityData entityData = entity.getEntityData();
-
-		if (entity instanceof RocketTier1Entity)
-		{
-			return entityData.get(RocketTier1Entity.FUEL);
-		}
-		else if (entity instanceof RocketTier2Entity)
-		{
-			return entityData.get(RocketTier2Entity.FUEL);
-		}
-		else if (entity instanceof RocketTier3Entity)
-		{
-			return entityData.get(RocketTier3Entity.FUEL);
-		}
-		else if (entity instanceof RocketTier4Entity)
-		{
-			return entityData.get(RocketTier4Entity.FUEL);
-		}
-
-		return 0;
+		return entityData.get(IRocketEntity.FUEL);
 	}
 
 	public static void setCurrentFuel(Entity entity, int fuel)
 	{
 		fuel = Mth.clamp(fuel, 0, getFuelCapacity(entity));
 		SynchedEntityData entityData = entity.getEntityData();
-
-		if (entity instanceof RocketTier1Entity)
-		{
-			entityData.set(RocketTier1Entity.FUEL, fuel);
-		}
-		else if (entity instanceof RocketTier2Entity)
-		{
-			entityData.set(RocketTier2Entity.FUEL, fuel);
-		}
-		else if (entity instanceof RocketTier3Entity)
-		{
-			entityData.set(RocketTier3Entity.FUEL, fuel);
-		}
-		else if (entity instanceof RocketTier4Entity)
-		{
-			entityData.set(RocketTier4Entity.FUEL, fuel);
-		}
-
+		entityData.set(IRocketEntity.FUEL, fuel);
 	}
 
 	public static int getFuelCapacity(Entity entity)
