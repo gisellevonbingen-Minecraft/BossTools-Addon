7a5c04caa4c6c2931d56d09a2a194531e1c97abd
gisellevonbingen <gisellevonbingen@gmail.com>
2022-03-18 11:14:57 +0900

    Update DeferredRegisterWrapper

--- src/main/java/beyond_earth_giselle_addon/common/registries/ItemDeferredRegister.java
+++ src/main/java/beyond_earth_giselle_addon/common/registries/ItemDeferredRegister.java
@@ -1,12 +1,13 @@
 package beyond_earth_giselle_addon.common.registries;
 
 import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class ItemDeferredRegister extends DeferredRegisterWrapper<Item>
 {
 	public ItemDeferredRegister(String modid)
 	{
-		super(modid, Item.class);
+		super(modid, ForgeRegistries.ITEMS);
 	}
 
 }
