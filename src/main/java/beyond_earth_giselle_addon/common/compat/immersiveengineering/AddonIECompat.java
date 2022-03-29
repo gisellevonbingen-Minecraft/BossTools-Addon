180bd24c24ff987668663ca1cb41d76f0f39b049
gisellevonbingen <gisellevonbingen@gmail.com>
2022-03-29 23:15:14 +0900

    Port Immersive Engineering Compat

--- src/main/java/beyond_earth_giselle_addon/common/compat/immersiveengineering/AddonIECompat.java
+++ src/main/java/beyond_earth_giselle_addon/common/compat/immersiveengineering/AddonIECompat.java
@@ -1,7 +1,11 @@
 package beyond_earth_giselle_addon.common.compat.immersiveengineering;
 
+import beyond_earth_giselle_addon.common.BeyondEarthAddon;
 import beyond_earth_giselle_addon.common.compat.AddonCompatibleMod;
+import blusunrize.immersiveengineering.api.IEApi;
+import blusunrize.immersiveengineering.api.crafting.ArcRecyclingChecker;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -26,8 +30,8 @@ public class AddonIECompat extends AddonCompatibleMod
 		IEventBus fml_bus = FMLJavaModLoadingContext.get().getModEventBus();
 		AddonIEItems.ITEMS.register(fml_bus);
 
-		// IEApi.prefixToIngotMap.put("compresseds", new Integer[]{1, 1});
-		// ArcRecyclingChecker.allowItemTagForRecycling(ItemTags.create(BeyondEarthAddon.prl("compresseds")));
+		IEApi.prefixToIngotMap.put("compresseds", new Integer[]{1, 1});
+		ArcRecyclingChecker.allowItemTagForRecycling(ItemTags.create(BeyondEarthAddon.prl("compresseds")));
 	}
 
 }
