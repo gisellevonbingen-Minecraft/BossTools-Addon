f4945cbefe3f312495fdc420b4d920fccfa09e19
gisellevonbingen <gisellevonbingen@gmail.com>
2022-12-22 10:00:06 +0900

    Enchantments need equip all armors of defence is least iron

---
+++ src/main/java/beyond_earth_giselle_addon/common/util/ArmorUtils.java
@@ -0,0 +1,61 @@
+package beyond_earth_giselle_addon.common.util;
+
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ArmorMaterials;
+import net.minecraft.world.item.ItemStack;
+
+public class ArmorUtils
+{
+
+	public static boolean allEquipLeastIron(LivingEntity living)
+	{
+		for (EquipmentSlot slot : EquipmentSlot.values())
+		{
+			if (slot.getType() == EquipmentSlot.Type.ARMOR)
+			{
+				ItemStack stack = living.getItemBySlot(slot);
+
+				if (isLeastIron(stack) == false)
+				{
+					return false;
+				}
+
+			}
+
+		}
+
+		return true;
+	}
+
+	public static boolean isLeastIron(ItemStack stack)
+	{
+		return getPrimaryDefence(stack) >= getPrimaryDefence(ArmorMaterials.IRON);
+	}
+
+	public static int getPrimaryDefence(ItemStack stack)
+	{
+		if (stack.getItem() instanceof ArmorItem armorItem)
+		{
+			return getPrimaryDefence(armorItem.getMaterial());
+		}
+		else
+		{
+			return 0;
+		}
+
+	}
+
+	public static int getPrimaryDefence(ArmorMaterial material)
+	{
+		return material.getDefenseForSlot(EquipmentSlot.CHEST);
+	}
+
+	private ArmorUtils()
+	{
+
+	}
+
+}
